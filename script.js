60d8d8b7c49c2401935b2011912833178d6bb350
bhushanverma1998 <bhushanverma.verma@gmail.com>
2023-01-06 13:50:51 +0530

    bugs fixed

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
+// Variable declaration and acccessing elements;
 let alarms = [];
 let currentTime = document.getElementById('current-time');
 let alarmInput = document.getElementById('alarm-input');
 let alarmList = document.getElementById('alarm-list');
 
-//IIFE function of display data first time.
-(
-    function(){
-        let data=localStorage.getItem('alarms');
-        if(data){
-            alarms=JSON.parse(data);
-        }
-        else{
-            localStorage.setItem('alarms',[]);
-        }
-        renderList();
-    }
-)();
+// display data first time from localstorage.
+let data = localStorage.getItem('alarms');
+if (data) {
+    alarms = JSON.parse(data);
+}
+else {
+    localStorage.setItem('alarms', []);
+}
+renderList();
 
 //Display current time on clock And check alarm
 setInterval(() => {
@@ -41,7 +38,7 @@ setInterval(() => {
     currentTime.innerHTML = `${hr}:${min}:${sec} ${zone}`;
 
     //Check for alarms alert
-    if(alarms[0]-curtime.getTime()<=0){
+    if (alarms[0] - curtime.getTime() <= 0) {
         alarmFire(alarms[0]);
     }
 }, 1000);
@@ -55,13 +52,13 @@ function renderList() {
         alarms.map((elem) => {
             let date = new Date(elem);
             let hr = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
-            hr=hr<10?"0"+hr:hr;
+            hr = hr < 10 ? "0" + hr : hr;
             list += `
         <div class="alarm-items">
         <div class="alarm-time">
-            <span>${hr==='00'?12:hr}</span>:
-            <span>${date.getMinutes()<10?"0"+date.getMinutes():date.getMinutes()}</span>:
-            <span>${date.getSeconds()<10?"0"+date.getSeconds():date.getSeconds()}</span>:
+            <span>${hr === '00' ? 12 : hr}</span>:
+            <span>${date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()}</span>:
+            <span>${date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds()}</span>:
             <span>${date.getHours() >= 12 ? "PM" : "AM"}</span>
         </div>
         <button class="delete-btn" id=${date.getTime()}>Delete</button>
@@ -81,7 +78,7 @@ function renderList() {
 function setAlarm(date) {
     alarms.push(date.getTime());
     alarms.sort();
-    localStorage.setItem('alarms',JSON.stringify(alarms));
+    localStorage.setItem('alarms', JSON.stringify(alarms));
     showNotification(`Alarm set Successfully`);
     renderList();
 }
@@ -92,32 +89,32 @@ function deleteAlarm(alarmId) {
         return elem !== Number(alarmId);
     });
     alarms = newList;
-    localStorage.setItem('alarms',JSON.stringify(alarms));
+    localStorage.setItem('alarms', JSON.stringify(alarms));
     showNotification("Alarm deleted");
     renderList();
 }
 
 //Trigger alarm alert when invoked
 function alarmFire(alarm) {
-    let newAlarms=alarms.slice(1);
-    alarms=newAlarms;
-    localStorage.setItem('alarms',JSON.stringify(alarms));
-    let response=confirm("Press Ok! to turn off alarm. Cancel to snooze for 5 minutes");
-    if(response){
+    let newAlarms = alarms.slice(1);
+    alarms = newAlarms;
+    localStorage.setItem('alarms', JSON.stringify(alarms));
+    let response = confirm("Press Ok! to turn off alarm. Cancel to snooze for 5 minutes");
+    if (response) {
         renderList();
     }
-    else{
+    else {
         snoozeAlarm(alarm);
     }
 }
 
 //Snooze alarm by 5 minutes
-function snoozeAlarm(alarm){ 
-    let time=new Date(alarm);
-    time.setMinutes(time.getMinutes()+5);
+function snoozeAlarm(alarm) {
+    let time = new Date(alarm);
+    time.setMinutes(time.getMinutes() + 5);
     alarms.push(time.getTime());
     alarms.sort();
-    localStorage.setItem('alarms',JSON.stringify(alarms));
+    localStorage.setItem('alarms', JSON.stringify(alarms));
     showNotification("Alarm snoozed to 5 minutes")
     renderList();
 }
@@ -137,22 +134,22 @@ document.addEventListener('click', (event) => {
         let userZone = document.getElementById('user-zone');
 
         //Check upper and lower conditions for valid time
-        if (userHr.value === '' || userMin.value === '' || userSec.value === '' || userZone.value === '' ||userHr.value < 0 || userHr.value>12 || userMin.value < 0 || userMin.value > 60 || userSec.value < 0 || userSec.value > 60) {
+        if (userHr.value === '' || userMin.value === '' || userSec.value === '' || userZone.value === '' || userHr.value < 0 || userHr.value > 12 || userMin.value < 0 || userMin.value > 60 || userSec.value < 0 || userSec.value > 60) {
             showNotification("Set a valid input in alarm");
-            userHr.value='';
-            userMin.value='';
-            userSec.value='';
-            userZone.value='';
+            userHr.value = '';
+            userMin.value = '';
+            userSec.value = '';
+            userZone.value = '';
             return;
         }
         else {
             //if zone is PM add 12 in hour
             let newHr
             if (userZone.value === "PM") {
-                newHr =Number(userHr.value)===12?Number(userHr.value):Number(userHr.value) + 12;
+                newHr = Number(userHr.value) === 12 ? Number(userHr.value) : Number(userHr.value) + 12;
             }
             else {
-                newHr =Number(userHr.value)===12?0: Number(userHr.value);
+                newHr = Number(userHr.value) === 12 ? 0 : Number(userHr.value);
             }
 
             //Set the alarm
@@ -167,10 +164,10 @@ document.addEventListener('click', (event) => {
                 date.setDate(date.getDate() + 1);
             }
             setAlarm(date);
-            userHr.value='';
-            userMin.value='';
-            userSec.value='';
-            userZone.value='';
+            userHr.value = '';
+            userMin.value = '';
+            userSec.value = '';
+            userZone.value = '';
         }
     }
     if (event.target.className === "delete-btn") {
